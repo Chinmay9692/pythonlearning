7426512f8e729ac02585cf303ace5d90ba2a39fc
Chinmay9692 <147390920+Chinmay9692@users.noreply.github.com>
2023-10-09 15:46:49 +0530

    Add files via upload

---
+++ Lab003.py
@@ -0,0 +1,12 @@
+# Task1: Print the Name of the user by taking it from the input command.
+username = input("Enter the user name:")
+print(username)
+
+# Task2: Take first name and last name of user and print with sep. = "-" and end with /t
+f_name = input("Enter the first name:")
+l_name = input("Enter the last name:")
+print(f_name, l_name, sep='-', end='\t')
+
+# Task3: Take a user input as name and say with message
+user_name = input("Enter the user name:")
+print("Hello", user_name, "How are you")
